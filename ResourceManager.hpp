62921d9bb3a755a525fa598d8662952a3b48a78c
SwwK2048 <88043842+SwwK2048@users.noreply.github.com>
2025-11-02 19:28:11 +0100

    commit2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -11,6 +11,15 @@ class ResourceManager
 
         ~ResourceManager() { delete resource; }
 
+        ResourceManager(const ResourceManager& kopiowane): resource(new Resource(*kopiowane.resource)) {}
+
+        ResourceManager& operator=(const ResourceManager& kopiowane) 
+        {
+            *resource = *kopiowane.resource;
+            return *this;
+        }
+
+
         double get() const {
             return resource->get();
     }
