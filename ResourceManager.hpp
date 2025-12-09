94b962da144966761e1af8993be2a803813fbc3a
Stanisław Walczak <osapo137@gmail.com>
2025-12-09 13:54:32 +0100

    commit3

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -13,9 +13,24 @@ class ResourceManager
 
         ResourceManager(const ResourceManager& kopiowane): resource(new Resource(*kopiowane.resource)) {}
 
+        ResourceManager(ResourceManager&& przenoszone): resource(przenoszone.resource)
+        {
+            przenoszone.resource = nullptr;
+        }
+
         ResourceManager& operator=(const ResourceManager& kopiowane) 
         {
             *resource = *kopiowane.resource;
+            return *this;
+        }
+
+        ResourceManager& operator=(ResourceManager&& przenoszone)
+        {
+            if (this != &przenoszone) {
+                delete resource; 
+                resource       = przenoszone.resource;
+                przenoszone.resource = nullptr;
+            }
             return *this;
         }
 
